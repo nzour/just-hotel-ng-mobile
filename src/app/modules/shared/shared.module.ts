495071229aba9ad932690690827d58ee6e77b6d4
zaur <zaur.nadiraliev.228@gmail.com>
2019-11-23 14:36:15 +0300

    Revert "Имплементировал базовую реализацию side-menu для приложения"
    Удалил side-menu

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -6,11 +6,9 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApiInterceptor } from './interceptors/api.interceptor';
-import { HeaderComponent } from './components/header/header.component';
-import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MenuComponent],
+  declarations: [],
   imports: [
     IonicModule,
     HttpClientModule,
@@ -28,8 +26,6 @@ import { MenuComponent } from './components/menu/menu.component';
     FormsModule,
     CommonModule,
     SharedServiceModule,
-    HeaderComponent,
-    MenuComponent
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }
