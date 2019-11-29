466d2dc0025a9714de7e5f78569d47215a5552cb
zaur <zaur.nadiraliev.228@gmail.com>
2019-11-29 23:38:37 +0300

    NotifierService - сделал все методы асинхронными

--- src/app/modules/shared/services/notifier.service.ts
+++ src/app/modules/shared/services/notifier.service.ts
@@ -7,31 +7,32 @@ export class NotifierService {
 
   constructor(private toasts: ToastController) { }
 
-  dispatchMessage(message: string, duration: number = 5000): void {
-    this.dispatch(message, 'light', duration);
+  async dispatchMessage(message: string, duration: number = 5000): Promise<void> {
+    await this.dispatch(message, 'light', duration);
   }
 
-  dispatchError(message: string, duration: number = 5000): void {
-    this.dispatch(message, 'danger', duration);
+  async dispatchError(message: string, duration: number = 5000): Promise<void> {
+    await this.dispatch(message, 'danger', duration);
   }
 
-  dispatchWarning(message: string, duration: number = 5000): void {
-    this.dispatch(message, 'warning', duration);
+  async dispatchWarning(message: string, duration: number = 5000): Promise<void> {
+    await this.dispatch(message, 'warning', duration);
   }
 
-  dispatchSuccess(message: string, duration: number = 5000): void {
-    this.dispatch(message, 'success', duration);
+  async dispatchSuccess(message: string, duration: number = 5000): Promise<void> {
+    await this.dispatch(message, 'success', duration);
   }
 
   async dispatch(message: string, color: IonicColor, duration: number = 5000): Promise<void> {
-    await this.toasts
+    const toast = await this.toasts
       .create({
         message: message,
         color: color,
         duration: duration,
         showCloseButton: true
-      })
-      .then(async toast => await toast.present());
+      });
+
+    await toast.present();
   }
 }
 
