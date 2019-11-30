e7dc3a100f6278cb8a4c7650cabda61f374c1bd3
zaur <zaur.nadiraliev.228@gmail.com>
2019-11-30 16:23:14 +0300

    Сделал компонент для footer-buttons

---
+++ src/app/modules/shared/footer-buttons/footer-buttons.component.ts
@@ -0,0 +1,61 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IonicColor } from '../types/manual';
+
+@Component({
+  selector: 'app-footer-buttons',
+  templateUrl: './footer-buttons.component.html',
+  styleUrls: ['./footer-buttons.component.scss'],
+})
+export class FooterButtonsComponent {
+
+  private _confirmButtonEmitter = new EventEmitter<void>();
+  private _cancelButtonEmitter = new EventEmitter<void>();
+
+  private _confirmButtonName = 'Подтвердить';
+
+  get confirmButtonName(): string {
+    return this._confirmButtonName;
+  }
+
+  @Input() set confirmButtonName(value: string) {
+    this._confirmButtonName = value;
+  }
+
+  private _cancelButtonName = 'Отмена';
+
+  get cancelButtonName(): string {
+    return this._cancelButtonName;
+  }
+
+  @Input() set cancelButtonName(value: string) {
+    this._cancelButtonName = value;
+  }
+
+  private _confirmButtonColor: IonicColor = 'primary';
+
+  get confirmButtonColor(): IonicColor {
+    return this._confirmButtonColor;
+  }
+
+  private _cancelButtonColor: IonicColor = 'light';
+
+  get cancelButtonColor(): IonicColor {
+    return this._cancelButtonColor;
+  }
+
+  @Output() get confirmButtonClick(): EventEmitter<void> {
+    return this._confirmButtonEmitter;
+  }
+
+  @Output() get cancelButtonClick(): EventEmitter<void> {
+    return this._cancelButtonEmitter;
+  }
+
+  emitConfirmButton(): void {
+    this._confirmButtonEmitter.emit();
+  }
+
+  emitCancelButton(): void {
+    this._cancelButtonEmitter.emit();
+  }
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -9,12 +9,14 @@ import { ApiInterceptor } from './interceptors/api.interceptor';
 import { CountablePipe } from './pipes/countable.pipe';
 import { RolePipe } from './pipes/role.pipe';
 import { RoomTypePipe } from './pipes/room-type.pipe';
+import { FooterButtonsComponent } from './footer-buttons/footer-buttons.component';
 
 @NgModule({
   declarations: [
     RolePipe,
     CountablePipe,
-    RoomTypePipe
+    RoomTypePipe,
+    FooterButtonsComponent
   ],
   imports: [
     IonicModule,
@@ -36,6 +38,7 @@ import { RoomTypePipe } from './pipes/room-type.pipe';
     CountablePipe,
     RolePipe,
     RoomTypePipe,
+    FooterButtonsComponent,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }
